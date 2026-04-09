7abd03cfd80535cd92ed461b9f23aaa5a9a5ee15
Kevin Aguilar <kevin.231@hotmail.com>
2026-04-09 15:54:18 -0600

    Add CPURegister and ProcessorMode enums

--- system/types.ts
+++ system/types.ts
@@ -3,3 +3,35 @@
 export type Unsigned32Bit = number;
 export type Unsigned16Bit = number;
 export type Unsigned8Bit = number;
+
+
+export enum CPURegister {
+  Data0 = 0,
+  Data1 = 1,
+  Data2 = 2,
+  Data3 = 3,
+  Data4 = 4,
+  Data5 = 5,
+  Data6 = 6,
+  Data7 = 7,
+  Data8 = 8,
+  Data9 = 9,
+  Data10 = 10,
+  Data11 = 11,
+  Data12 = 12,
+
+  StackPointer = 13,
+  LinkRegister = 14,
+  ProgramCounter = 15,
+  StatusRegister = 16
+}
+
+export enum ProcessorMode {
+  User = 0x10,
+  FastInterrupt = 0x11,
+  NormalInterrupt = 0x12,
+  Supervisor = 0x13,
+  MemoryAbort = 0x17,
+  UndefinedInstr = 0x1B,
+  System = 0x1F
+}
